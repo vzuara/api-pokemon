bd36d463a2ec6cbedc6cbad677fb4294653c5875
Victor Azuara <vzuara@gmail.com>
2022-08-13 16:20:08 -0700

    Mongo CRUD

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    MongooseModule.forRoot(
+      'mongodb+srv://dbUser:FMHF*_wKx7E-iAi@cluster0.205fs.mongodb.net/nest-pokemon',
+    ),
+    PokemonModule,
+    CommonModule,
+  ],
+})
+export class AppModule {}
---
+++ src/common/pipes/parse-mongo-id.pipe.ts
@@ -0,0 +1,13 @@
+import { Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
+import { isValidObjectId } from 'mongoose';
+
+@Injectable()
+export class ParseMongoIdPipe implements PipeTransform {
+  transform(value: string) {
+    if (!isValidObjectId(value)) {
+      throw new BadRequestException(`${value} is not a valid MongoID`);
+    }
+
+    return value;
+  }
+}
